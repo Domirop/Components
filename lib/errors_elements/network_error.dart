67c95bdda935662861d72a6167525cf0e0b0c0a1
Domirop <59377864+Domirop@users.noreply.github.com>
2020-09-29 11:55:50 +0100

    cut svg

--- lib/errors_elements/network_error.dart
+++ lib/errors_elements/network_error.dart
@@ -1,17 +1,17 @@
 import 'package:components/text_types/large_text.dart';
 import 'package:components/text_types/medium_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class NetworkError extends StatelessWidget {
   double height;
   double width;
+  Widget widget;
   LargeText largeText;
   MediumText mediumText1;
   MediumText mediumText2;
 
   NetworkError(this.height, this.width, this.largeText, this.mediumText1,
-      this.mediumText2);
+      this.mediumText2, this.widget);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +23,7 @@ class NetworkError extends StatelessWidget {
             largeText,
             Padding(
               padding: EdgeInsets.only(top: height * 0.05),
-              child: SvgPicture.asset("assets/images/ufo.svg"),
+              child: widget,
             ),
             Padding(
               padding: EdgeInsets.only(top: height * 0.05),
